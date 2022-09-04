f0a384f3602a247aa178154d9fde03d96e73d5e5
Michael Gaynor <mgaynor228@gmail.com>
2022-09-03 21:21:25 -0400

    Changethe getElementId and addEventListener 'keyup' to the code

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
-const square = document.getElementsByClassName("square");
-square.addEventListener("mouseover",function(){
-    square.style.color = "blue"
-})
+document.getElementById("square").addEventListener('mouseenter', function() {
+    this.style.backgroundColor = "blue"
+});
 
-square.addEventListener("mousedown", function(){
-    square.style.color = "red"
-})
+document.getElementById("square").addEventListener('mousedown', function(){
+    this.style.backgroundColor = "red"
+});
 
-square.addEventListener("mouseup", function(){
-    square.style.color = "yellow"
-})
+document.getElementById("square").addEventListener('mouseup', function(){
+    this.style.backgroundColor = "yellow"
+});
 
-square.addEventListener("dbclick", function(){
-    square.style.color = "green"
-})
+document.getElementById("square").addEventListener('dbclick', function(){
+    this.style.backgroundColor = "green"
+});
 
-square.addEventListener("wheel", function(){
-    square.style.color = "orange"
-})
+document.getElementById("square").addEventListener('wheel', function(){
+    this.style.backgroundColor = "orange"
+});
 
+
+// Key Press for all the colors 
+document.addEventListener('keyup', function(event) {
+    if (event.key == 'r') {
+        document.getElementById("square").style.backgroundColor = "red"
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.key == 'b') {
+        document.getElementById("square").style.backgroundColor = "blue"
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.key == 'g') {
+        document.getElementById("square").style.backgroundColor = "green"
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    if (event.key == 'o') {
+        document.getElementById("square").style.backgroundColor = "orange"
+    }
+});
